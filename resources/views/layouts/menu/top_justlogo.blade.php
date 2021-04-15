3e81110b5429368ef739155a2e86238ac67427ee
Marcel Nagm <marcel.nagm@gmail.com>
2021-04-15 16:58:30 -0400

    modificação para menu em cima

--- resources/views/layouts/menu/top_justlogo.blade.php
+++ resources/views/layouts/menu/top_justlogo.blade.php
@@ -3,142 +3,149 @@
 
     <div class="container-fluid">
         @if(!config('settings.hide_project_branding'))
-          <a class="navbar-brand mr-lg-5" href="/">
+        <a class="navbar-brand mr-lg-5" href="/">
             <img src="{{ config('global.site_logo') }}">
-          </a>
+        </a>
         @else
         <a class="navbar-brand mr-lg-5" href="/">
-          
+
         </a>
         @endif
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
 
 
         <div class="navbar-collapse collapse" id="navbar_global">
-          <div class="navbar-collapse-header">
-            <div class="row">
-              @if(!config('settings.hide_project_branding'))
-              <div class="col-6 collapse-brand">
-                <a href="#">
-                  <img src="{{ config('global.site_logo') }}">
-                </a>
-              </div>
-              @else
-              <div class="col-6 collapse-brand">
-                <a href="#">
-                  
-                </a>
-              </div>
-              @endif
-              <div class="col-6 collapse-close">
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
-                  <span></span>
-                  <span></span>
-                </button>
-              </div>
-            </div>
-          </div>
+            <div class="navbar-collapse-header">
+                <div class="row">
+                    @if(!config('settings.hide_project_branding'))
+                    <div class="col-6 collapse-brand">
+                        <a href="#">
+                            <img src="{{ config('global.site_logo') }}">
+                        </a>
+                    </div>
+                    @else
+                    <div class="col-6 collapse-brand">
+                        <a href="#">
 
-          <ul class="navbar-nav align-items-lg-center ml-lg-auto">
-            @isset($restorant)
+                        </a>
+                    </div>
+                    @endif
+                    <div class="col-6 collapse-close">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
+                            <span></span>
+                            <span></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+                @isset($restorant)
                 @yield('addiitional_button_1')
                 @yield('addiitional_button_2')
-                  
-                  @if(config('settings.enable_call_waiter') && strlen(config('broadcasting.connections.pusher.app_id')) > 2 && strlen(config('broadcasting.connections.pusher.key')) > 2 && strlen(config('broadcasting.connections.pusher.secret')) > 2)
-                    <li class="web-menu mr-1">
-                      <button type="button" class="btn btn-neutral btn-icon btn-cart" data-toggle="modal" data-target="#modal-form">
+                <li class="nav-item dropdown">
+                    @auth()
+                    @include('layouts.menu.partials.auth')
+                    @endauth
+                    @guest()
+                    @include('layouts.menu.partials.guest')
+                    @endguest
+                </li>
+                @if(config('settings.enable_call_waiter') && strlen(config('broadcasting.connections.pusher.app_id')) > 2 && strlen(config('broadcasting.connections.pusher.key')) > 2 && strlen(config('broadcasting.connections.pusher.secret')) > 2)
+                <li class="web-menu mr-1">
+                    <button type="button" class="btn btn-neutral btn-icon btn-cart" data-toggle="modal" data-target="#modal-form">
                         <span class="btn-inner--icon">
-                          <i class="fa fa-bell"></i>
+                            <i class="fa fa-bell"></i>
                         </span>
                         <span class="nav-link-inner--text">{{ __('Call Waiter') }}</span>
-                      </button>
-                    </li>
-                    <li class="web-menu mr-1">
-                      <a  href="{{ route('register.visit',['restaurant_id'=>$restorant->id])}}" class="btn btn-neutral btn-icon btn-cart" style="cursor:pointer;">
-                            <span class="btn-inner--icon">
-                              <i class="fa fa-calendar-plus-o"></i>
-                            </span>
-                            <span class="nav-link-inner--text">{{ __('Register visit') }}</span>
-                        </a>
-                    </li>
-                  @endif
-
-                  @if(isset($hasGuestOrders)&&$hasGuestOrders)
-                    <li class="web-menu mr-1">
-                      <a  href="{{ route('guest.orders')}}" class="btn btn-neutral btn-icon btn-cart" style="cursor:pointer;">
+                    </button>
+                </li>
+                <li class="web-menu mr-1">
+                    <a  href="{{ route('register.visit',['restaurant_id'=>$restorant->id])}}" class="btn btn-neutral btn-icon btn-cart" style="cursor:pointer;">
                         <span class="btn-inner--icon">
-                          <i class="fa fa-list-alt"></i>
+                            <i class="fa fa-calendar-plus-o"></i>
+                        </span>
+                        <span class="nav-link-inner--text">{{ __('Register visit') }}</span>
+                    </a>
+                </li>
+                @endif
+
+                @if(isset($hasGuestOrders)&&$hasGuestOrders)
+                <li class="web-menu mr-1">
+                    <a  href="{{ route('guest.orders')}}" class="btn btn-neutral btn-icon btn-cart" style="cursor:pointer;">
+                        <span class="btn-inner--icon">
+                            <i class="fa fa-list-alt"></i>
                         </span>
                         <span class="nav-link-inner--text">{{ __('My Orders') }}</span>
-                      </a>
-                    </li>
-                  @endif
+                    </a>
+                </li>
+                @endif
 
 
-            @endisset
+                @endisset
 
 
-            @if(\Request::route()->getName() != "newrestaurant.register" && config('app.ordering'))
-            <li class="web-menu">
+                @if(\Request::route()->getName() != "newrestaurant.register" && config('app.ordering'))
+                <li class="web-menu">
 
-              @if(\Request::route()->getName() != "cart.checkout")
-                <a  id="desCartLink" onclick="openNav()" class="btn btn-neutral btn-icon btn-cart" style="cursor:pointer;">
-                  <span class="btn-inner--icon">
-                    <i class="fa fa-shopping-cart"></i>
-                  </span>
-                  <span class="nav-link-inner--text">{{ __('Cart') }}</span>
-              </a>
-              @endif
+                    @if(\Request::route()->getName() != "cart.checkout")
+                    <a  id="desCartLink" onclick="openNav()" class="btn btn-neutral btn-icon btn-cart" style="cursor:pointer;">
+                        <span class="btn-inner--icon">
+                            <i class="fa fa-shopping-cart"></i>
+                        </span>
+                        <span class="nav-link-inner--text">{{ __('Cart') }}</span>
+                    </a>
+                    @endif
 
-            </li>
-            @endif
-            <li class="mobile-menu">
+                </li>
+                @endif
+                <li class="mobile-menu">
 
-              @yield('addiitional_button_1_mobile')
-              @yield('addiitional_button_2_mobile')
+                    @yield('addiitional_button_1_mobile')
+                    @yield('addiitional_button_2_mobile')
 
-              @isset($restorant)
-                
-                  @if(config('settings.enable_call_waiter') && strlen(config('broadcasting.connections.pusher.app_id')) > 2 && strlen(config('broadcasting.connections.pusher.key')) > 2 && strlen(config('broadcasting.connections.pusher.secret')) > 2)
+                    @isset($restorant)
+
+                    @if(config('settings.enable_call_waiter') && strlen(config('broadcasting.connections.pusher.app_id')) > 2 && strlen(config('broadcasting.connections.pusher.key')) > 2 && strlen(config('broadcasting.connections.pusher.secret')) > 2)
                     <a type="button" class="nav-link" data-toggle="modal" data-target="#modal-form">
-                      <span class="btn-inner--icon">
-                        <i class="fa fa-bell"></i>
-                      </span>
-                      <span class="nav-link-inner--text">{{ __('Call Waiter') }}</span>
+                        <span class="btn-inner--icon">
+                            <i class="fa fa-bell"></i>
+                        </span>
+                        <span class="nav-link-inner--text">{{ __('Call Waiter') }}</span>
                     </a>
                     <a href="{{ route('register.visit',['restaurant_id'=>$restorant->id])}}" class="nav-link" style="cursor:pointer;">
                         <i class="fa fa-calendar-plus-o"></i>
                         <span class="nav-link-inner--text">{{ __('Register visit') }}</span>
                     </a>
-                  @endif
+                    @endif
 
-                  @if(isset($hasGuestOrders)&&$hasGuestOrders)
+                    @if(isset($hasGuestOrders)&&$hasGuestOrders)
 
                     <a  href="{{ route('guest.orders')}}" class="nav-link" style="cursor:pointer;">
 
                         <i class="fa fa-list-alt"></i>
 
-                      <span class="nav-link-inner--text">{{ __('My Orders') }}</span>
+                        <span class="nav-link-inner--text">{{ __('My Orders') }}</span>
                     </a>
-                  @endif
-                @endif
+                    @endif
+                    @endif
 
-                @if(\Request::route()->getName() != "newrestaurant.register" && config('app.ordering'))
-                <a id="mobileCartLink" onclick="openNav()" class="nav-link" style="cursor:pointer;">
-                    <i class="fa fa-shopping-cart"></i>
-                    <span class="nav-link-inner--text">{{ __('Cart') }}</span>
-                </a>
-              @endisset
+                    @if(\Request::route()->getName() != "newrestaurant.register" && config('app.ordering'))
+                    <a id="mobileCartLink" onclick="openNav()" class="nav-link" style="cursor:pointer;">
+                        <i class="fa fa-shopping-cart"></i>
+                        <span class="nav-link-inner--text">{{ __('Cart') }}</span>
+                    </a>
+                    @endisset
 
 
-            </li>
-          </ul>
+                </li>
+            </ul>
         </div>
 
 
-      </div>
+    </div>
 
-    </nav>
+</nav>
