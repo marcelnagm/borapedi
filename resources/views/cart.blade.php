4febf080d8d8d4f1a5382e33fbaf33d78bc91c87
Marcel Nagm <marcel.nagm@gmail.com>
2021-04-16 13:11:03 -0400

    Ajustes no carrinho para pegar todas as opções

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,141 +1,147 @@
 @extends('layouts.front', ['class' => ''])
 @section('content')
-    <section class="section-profile-cover section-shaped my--1 d-none d-md-none d-lg-block d-lx-block">
-        <!-- Circles background -->
-        <img class="bg-image " src="{{ config('global.restorant_details_cover_image') }}" style="width: 100%;">
-        <!-- SVG separator -->
-        <div class="separator separator-bottom separator-skew">
+<section class="section-profile-cover section-shaped my--1 d-none d-md-none d-lg-block d-lx-block">
+    <!-- Circles background -->
+    <img class="bg-image " src="{{ config('global.restorant_details_cover_image') }}" style="width: 100%;">
+    <!-- SVG separator -->
+    <div class="separator separator-bottom separator-skew">
 
-        </div>
-    </section>
-    <section class="section bg-secondary">
+    </div>
+</section>
+<section class="section bg-secondary">
 
-      <div class="container">
+    <div class="container">
 
 
         <x:notify-messages />
 
-          <div class="row">
+        <div class="row">
 
             <!-- Left part -->
             <div class="col-md-7">
 
-              <!-- List of items -->
-              @include('cart.items')
+                <!-- List of items -->
+                @include('cart.items')
 
                 <form id="order-form" role="form" method="post" action="{{route('order.store')}}" autocomplete="off" enctype="multipart/form-data">
-                @csrf
-                @if(!config('settings.social_mode'))
+                    @csrf
+                    @if(!config('settings.social_mode'))
 
                     @if (config('app.isft')&&count($timeSlots)>0)
                     <!-- FOOD TIGER -->
-                        <!-- Delivery method -->
-                        @if($restorant->can_pickup == 1)
-                            @if($restorant->can_deliver == 1)
-                              @include('cart.delivery')
-                            @endif
-                        @endif
+                    <!-- Delivery method -->
+                    @if($restorant->can_pickup == 1)
+                    @if($restorant->can_deliver == 1)
+                    @include('cart.delivery')
+                    @endif
+                    @endif
+                    <!-- Delivery address -->
+                    <div id='addressBox'>
+                        @include('cart.address')
+                    </div>
 
-                        <!-- Delivery address -->
-                        <div id='addressBox'>
-                            @include('cart.address')
-                        </div>
-
-                        <!-- Comment -->
-                        @include('cart.comment')
+                    <!-- Comment -->
+                    @include('cart.comment')
 
                     @else
 
-                      <!-- QRSAAS -->
-                      
-                      <!-- DINE IN OR TAKEAWAY -->
-                      @if (config('settings.enable_pickup'))
-                          @include('cart.localorder.dineiintakeaway')
-                          <!-- Takeaway time slot -->
-                          <div class="takeaway_picker" style="display: none">
-                              @include('cart.time')
-                          </div>
-                      @endif
+                    <!-- QRSAAS -->
 
-                      <!-- LOCAL ORDERING -->
+                    
+
+                    <!-- LOCAL ORDERING -->
                       @include('cart.localorder.table')
 
                       <!-- Local Order Phone -->
                       @include('cart.localorder.phone')
 
-                      <!-- Custom Fields -->
-                      @include('cart.customfields')
+                    <!-- Custom Fields -->
+                    @include('cart.customfields')
 
-                      <!-- Comment -->
-                      @include('cart.comment')
-                        
+                    <!-- Comment -->
+                    @include('cart.comment')
+
 
                     @endif
-                @else
+                    @else
                     <!-- Social MODE -->
 
                     @if(count($timeSlots)>0)
-                        <!-- Delivery method -->
-                        @include('cart.delivery')
+                    <!-- Delivery method -->
+                    @include('cart.delivery')
 
 
-                        <!-- Custom Fields  -->
-                        @include('cart.customfields')
+                    <!-- Custom Fields  -->
+                    @include('cart.customfields')
 
-                        <!-- Delivery adress -->
-                        @include('cart.newaddress')
+                    <!-- Delivery adress -->
+                    <div id='addressBox'>
+                        @include('cart.address')
+                    </div>
+                   <!-- DINE IN OR TAKEAWAY -->
+                    @if (config('settings.enable_pickup'))
+                    <!-- Takeaway time slot -->
+                    <div class="takeaway_picker" style="display: none">
+                        @include('cart.time')
+                          @include('cart.localorder.table')
 
-                        <!-- Comment -->
-                        @include('cart.comment')
+                      <!-- Local Order Phone -->
+                      @include('cart.localorder.phone')
+
+                    </div>
+
                     @endif
-                @endif
+                    <!-- Comment -->
+                    @include('cart.comment')
+                    @endif
+                    @endif
 
-              <!-- Restaurant -->
-              @include('cart.restaurant')
+                    <!-- Restaurant -->
+                    @include('cart.restaurant')
             </div>
 
 
-          <!-- Right Part -->
-          <div class="col-md-5">
+            <!-- Right Part -->
+            <div class="col-md-5">
 
-            @if (count($timeSlots)>0)
+                @if (count($timeSlots)>0)
                 <!-- Payment -->
                 @include('cart.payment')
                 <!--
                   <br/>
                   @include('cart.coupons')
                 -->
-            @else
+                @else
                 <!-- Closed restaurant -->
                 @include('cart.closed')
-            @endif
+                @endif
 
 
-          </div>
+            </div>
         </div>
 
 
     </div>
     @include('clients.modals')
-  </section>
+</section>
 @endsection
 @section('js')
 
-  <script async defer src= "https://maps.googleapis.com/maps/api/js?key=<?php echo config('settings.google_maps_api_key'); ?>&callback=initAddressMap"></script>
-  <!-- Stripe -->
-  <script src="https://js.stripe.com/v3/"></script>
-  <script>
-    "use strict";
-    var RESTORANT = <?php echo json_encode($restorant) ?>;
-    var STRIPE_KEY="{{ config('settings.stripe_key') }}";
-    var ENABLE_STRIPE="{{ config('settings.enable_stripe') }}";
-    var initialOrderType = 'delivery';
-    if(RESTORANT.can_deliver == 1 && RESTORANT.can_pickup == 0){
-        initialOrderType = 'delivery';
-    }else if(RESTORANT.can_deliver == 0 && RESTORANT.can_pickup == 1){
-        initialOrderType = 'pickup';
-    }
-  </script>
-  <script src="{{ asset('custom') }}/js/checkout.js"></script>
+<script async defer src= "https://maps.googleapis.com/maps/api/js?key=<?php echo config('settings.google_maps_api_key'); ?>&callback=initAddressMap"></script>
+<!-- Stripe -->
+<script src="https://js.stripe.com/v3/"></script>
+<script>
+"use strict";
+var RESTORANT = <?php echo json_encode($restorant) ?>;
+var STRIPE_KEY = "{{ config('settings.stripe_key') }}";
+var ENABLE_STRIPE = "{{ config('settings.enable_stripe') }}";
+var initialOrderType = 'delivery';
+if (RESTORANT.can_deliver == 1 && RESTORANT.can_pickup == 0) {
+  initialOrderType = 'delivery';
+} else if (RESTORANT.can_deliver == 0 && RESTORANT.can_pickup == 1) {
+  initialOrderType = 'pickup';
+}
+</script>
+<script src="{{ asset('custom') }}/js/checkout.js"></script>
 @endsection
 
--- resources/views/cart/delivery.blade.php
+++ resources/views/cart/delivery.blade.php
@@ -14,7 +14,23 @@
           <input name="deliveryType" class="custom-control-input" id="deliveryTypePickup" type="radio" value="pickup">
           <label class="custom-control-label" for="deliveryTypePickup">{{ __('Pickup') }}</label>
         </div>
+        <div class="mt-5">
+        <h3>{{ __('Dine In / Takeaway') }}<span class="font-weight-light"></span></h3>
+      </div>
+      <div class="card-content border-top">
+        <br />
+       
+        <div class="custom-control custom-radio mb-3">
+          <input name="deliveryType" class="custom-control-input" id="deliveryTypeDinein" type="radio" value="dinein" checked>
+          <label class="custom-control-label" for="deliveryTypeDinein">{{ __('Dine In') }}</label>
+        </div>
+        <div class="custom-control custom-radio mb-3">
+          <input name="deliveryType" class="custom-control-input" id="deliveryTypeTakeAway" type="radio" value="takeaway">
+          <label class="custom-control-label" for="deliveryTypeTakeAway">{{ __('Takeaway') }}</label>
+        </div>
 
+      </div>
+      <br />
       </div>
       <br />
       <br />
