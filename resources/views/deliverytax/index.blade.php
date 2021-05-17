3628f033a85a8b58da5c0f1f822f52315e2f99c4
Marcel Nagm <marcel.nagm@gmail.com>
2021-05-17 12:26:57 -0400

    fix para delivery tax

--- resources/views/deliverytax/index.blade.php
+++ resources/views/deliverytax/index.blade.php
@@ -61,7 +61,7 @@
 <!-- Google Map -->
 <!--<script async defer src= "https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=<?php // echo config('settings.google_maps_api_key');   ?>"></script>-->
 
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?key=<?php echo config('settings.google_maps_api_key'); ?>"></script>
+<script type="text/javascript" src="https://maps.google.com/maps/api/js?key=<?php echo config('settings.google_maps_api_key'); ?>"></script>
 <script type="text/javascript">
 
 var lat = <?php echo $lat ?>;
