d81ff4e1ee3bda74768d05f45b4f2835be76f771
Marcel Nagm <marcel.nagm@gmail.com>
2021-05-27 11:25:35 -0400

    Ajuste no modal de endereco

--- resources/views/clients/modals.blade.php
+++ resources/views/clients/modals.blade.php
@@ -1,3 +1,16 @@
+<style>
+    .cep-form{
+            float: left;
+    width: 78% !important;
+    }
+    .cep-label{
+            float: left;
+            height: 20px;
+    }
+    
+</style>
+
+
 <div class="modal fade" id="modal-order-new-address" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered modal-large" role="document">
         <div class="modal-content">
@@ -12,24 +25,18 @@
                     <div class="card-body px-lg-5 py-lg-5" id="new_address_checkout_body">
                         <div class="card-content border-top">
                             <br />
-                            <div class="row">
+                            <div class="row" style="margin-bottom:10px;">
                                 <div class="col-12">
-                                    @include('partials.fields',['fields'=>[
-                                    ['ftype'=>'input','name'=>"cep",'id'=>"cep",'placeholder'=>"Coloque seu CEP aqui ...",'required'=>true]
-                                    ]])
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-12">
-                                    </br>                                    
-                                    <a  alt="Não sei Meu Cef"  target="_blank"  href="https://buscacepinter.correios.com.br/app/endereco/index.php?t" class="btn btn-primary" >Não sei meu CEP</a>                                    
-                                    <div class="input-group mb-4">                                        
-                                        <button type="button" class="input-group-append button" onclick="getAdd()">
-                                            <span class="input-group-text"><i id="search_location" class="fa fa-map-pin" data-toggle="tooltip" data-placement="top" title="" data-original-title="Get my current location"></i></span>
-                                        </button>
-                                    </div>
-                                </div>
+                                    <div id="form-group-cep" class="form-group  ">
+                                        <label class="form-control-label cep-label" for="cep" style="position: relative; top: -22px;">Cep:</label>
+                                        <input step=".01" type="text" name="Cep" id="cep" class="form-control form-control cep-form  " placeholder="Coloque seu CEP aqui ..." value="" required="" autofocus="">
+                                        <button type="button" style="float:left; " onclick="getAdd()">
+                                        <span class="input-group-text cep-label"><i id="search_location" class="fa fa-map-pin" data-toggle="tooltip" data-placement="top" title="" data-original-title="Get my current location"></i></span>
+                                    </button>                                    
+                                        <a  alt="Não sei Meu Cef"  target="_blank"  href="https://buscacepinter.correios.com.br/app/endereco/index.php?t" class="btn-primary" style="float: left; " ><img src="/images/icons/naosei.png"></a>
+                                    </div>                                                                 
+                                </div>                               
+                                    
                             </div>
                             <div class="row">
                                 <div class="col-12">
@@ -63,16 +70,16 @@
                             </div>
                             <div class="row">
                                 <div class="col-12">
-                                  @include('partials.fields',['fields'=>[            
-                            ['ftype'=>'input','name'=>"Número",'id'=>"numbero",'placeholder'=>"Numero",'required'=>true],
-                            ]])
+                                    @include('partials.fields',['fields'=>[            
+                                    ['ftype'=>'input','name'=>"Número",'id'=>"numbero",'placeholder'=>"Numero",'required'=>true],
+                                    ]])
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-12">
-                             @include('partials.fields',['fields'=>[            
-                            ['ftype'=>'input','name'=>"complemento",'id'=>"complement",'placeholder'=>"Apartamento, Casa, e etc..'",'required'=>true]
-                            ]])   </div>
+                                    @include('partials.fields',['fields'=>[            
+                                    ['ftype'=>'input','name'=>"complemento",'id'=>"complement",'placeholder'=>"Apartamento, Casa, e etc..'",'required'=>true]
+                                    ]])   </div>
                             </div>
                             <input type="hidden"  name="phone_send"  id="phone_send" value="" > 
                         </div>
@@ -96,90 +103,90 @@
 crossorigin="anonymous"></script>
 <script>
 
-                                            function getAdd() {
-                                                if (navigator.geolocation) {
-                                                    navigator.geolocation.getCurrentPosition(function (position) {
-                                                        var pos = {lat: position.coords.latitude, lng: position.coords.longitude};
-                                                        $.ajaxSetup({
-                                                            headers: {
-                                                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                                                            }
-                                                        });
-                                                        $.ajax({
-                                                            type: 'POST',
-                                                            url: '/search/location',
-                                                            dataType: 'json',
-                                                            data: {
-                                                                lat: position.coords.latitude,
-                                                                lng: position.coords.longitude
-                                                            },
-                                                            success: function (response) {
-                                                                if (response.status) {
-                                                                    console.log(response.data);
-                                                                    $("#txtlocation").val(response.data.formatted_address);
-                                                                    $("#address").val(response.data.address_components[1]['long_name']);
-                                                                    $("#address_neigh").val(response.data.address_components[2]['long_name']);
-                                                                    $("#address_city").val(response.data.address_components[3]['long_name'] + ' - ' + response.data.address_components[4]['long_name']);
-                                                                    $("#numbero").val(response.data.address_components[0]['long_name']);
-                                                                    $("#cep").val(response.data.address_components[6]['long_name']);
-                                                                    $("#submitNewAddress").show();
-                                                                }
-                                                            }, error: function (response) {
-                                                            }
-                                                        });
+                                        function getAdd() {
+                                            if (navigator.geolocation) {
+                                                navigator.geolocation.getCurrentPosition(function (position) {
+                                                    var pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+                                                    $.ajaxSetup({
+                                                        headers: {
+                                                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                                        }
                                                     });
-                                                } else {
+                                                    $.ajax({
+                                                        type: 'POST',
+                                                        url: '/search/location',
+                                                        dataType: 'json',
+                                                        data: {
+                                                            lat: position.coords.latitude,
+                                                            lng: position.coords.longitude
+                                                        },
+                                                        success: function (response) {
+                                                            if (response.status) {
+                                                                console.log(response.data);
+                                                                $("#txtlocation").val(response.data.formatted_address);
+                                                                $("#address").val(response.data.address_components[1]['long_name']);
+                                                                $("#address_neigh").val(response.data.address_components[2]['long_name']);
+                                                                $("#address_city").val(response.data.address_components[3]['long_name'] + ' - ' + response.data.address_components[4]['long_name']);
+                                                                $("#numbero").val(response.data.address_components[0]['long_name']);
+                                                                $("#cep").val(response.data.address_components[6]['long_name']);
+                                                                $("#submitNewAddress").show();
+                                                            }
+                                                        }, error: function (response) {
+                                                        }
+                                                    });
+                                                });
+                                            } else {
 //                                                 Browser doesn't support Geolocation
 //                                                handleLocationError(false, infoWindow, map.getCenter());
-                                                }
                                             }
+                                        }
 
 
-                                            $(document).ready(function ($) {
+                                        $(document).ready(function ($) {
 
-                                                //Quando o campo cep perde o foco.
-                                                $("#cep").blur(function () {
+                                            //Quando o campo cep perde o foco.
+                                            $("#cep").blur(function () {
 //Nova variável "cep" somente com dígitos.
-                                                    var cep = $(this).val().replace(/\D/g, '');
-                                                    cep = cep.replace('-', '');
-                                                    $("#numbero").val('');
-                                                    //Verifica se campo cep possui valor informado.
-                                                    if (cep != "") {
+                                                var cep = $(this).val().replace(/\D/g, '');
+                                                cep = cep.replace('-', '');
+                                                $("#numbero").val('');
+                                                //Verifica se campo cep possui valor informado.
+                                                if (cep != "") {
 
 //Expressão regular para validar o CEP.
-                                                        var validacep = /^[0-9]{8}$/;
-                                                        //Valida o formato do CEP.
-                                                        if (validacep.test(cep)) {
+                                                    var validacep = /^[0-9]{8}$/;
+                                                    //Valida o formato do CEP.
+                                                    if (validacep.test(cep)) {
 
 //Preenche os campos com "..." enquanto consulta webservice.
 
 //Consulta o webservice viacep.com.br/
-                                                            $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+                                                        $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
 
-                                                                if (!("erro" in dados)) {
+                                                            if (!("erro" in dados)) {
 //Atualiza os campos com os valores da consulta.
-                                                                    $("#address").val(dados.logradouro);
-                                                                    $("#address_neigh").val(dados.bairro);
-                                                                    $("#address_city").val(dados.localidade + ' - ' + dados.uf);
-                                                                    $("#submitNewAddress").show();
-                                                                } //end if.
-                                                                else {
+                                                                $("#address").val(dados.logradouro);
+                                                                $("#address_neigh").val(dados.bairro);
+                                                                $("#address_city").val(dados.localidade + ' - ' + dados.uf);
+                                                                $("#submitNewAddress").show();
+                                                            } //end if.
+                                                            else {
 //CEP pesquisado não foi encontrado.                              
-                                                                    alert("CEP não encontrado.");
-                                                                }
-                                                            });
-                                                        } //end if.
-                                                        else {
-//cep é inválido.                      
-                                                            alert("Formato de CEP inválido.");
-                                                        }
+                                                                alert("CEP não encontrado.");
+                                                            }
+                                                        });
                                                     } //end if.
                                                     else {
+//cep é inválido.                      
+                                                        alert("Formato de CEP inválido.");
+                                                    }
+                                                } //end if.
+                                                else {
 //cep sem valor, limpa formulário.
 
-                                                    }
-                                                });
-                                            }
-                                            );
+                                                }
+                                            });
+                                        }
+                                        );
 
 </script>   
