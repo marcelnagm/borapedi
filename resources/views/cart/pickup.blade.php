21e072315c8d9aa71034362e855439e93bf281bf
Marcel Nagm <marcel.nagm@gmail.com>
2021-06-01 15:37:15 -0400

    habilita apenas consumo se estiver habilitado no plano

--- resources/views/cart/pickup.blade.php
+++ resources/views/cart/pickup.blade.php
@@ -3,9 +3,16 @@
         <input name="deliveryType" class="custom-control-input" id="deliveryTypePickup" type="radio" value="pickup">
         <label class="custom-control-label" for="deliveryTypePickup">{{ __('Pickup') }}</label>
     </div>
+
+    @if(auth()->user()->plan()->count() !=  0)
+
+
+
+    @if(auth()->user()->plan()->first()->local_table)
     <div class="custom-control custom-radio mb-3 col-4">
         <input name="deliveryType" class="custom-control-input" id="deliveryTypeDinein" type="radio" value="dinein" >
         <label class="custom-control-label" for="deliveryTypeDinein">Consumo no Local</label>
     </div>
-
+    @endif
+    @endif
 </div>
