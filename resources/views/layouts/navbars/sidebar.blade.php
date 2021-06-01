63e75dfff0661aaf3fce8f151cbdbf2b26f16859
Marcel Nagm <marcel.nagm@gmail.com>
2021-06-01 16:06:52 -0400

    redireciona caso tenha acessado um restauramte e faca o login redireciona para o restaurante visitado

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -5,7 +5,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <!-- Brand -->
-        <a class="navbar-brand pt-0" href="/">
+        <a class="navbar-brand pt-0" href="/home">
             <img src="{{ config('global.site_logo') }}" class="navbar-brand-img" alt="...">
         </a>
         <!-- User -->
