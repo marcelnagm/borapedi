34a7f3f913576610c3a2292d7f9662330646bed4
Marcel Nagm <marcel.nagm@gmail.com>
2021-07-02 21:10:10 -0400

    fix do show restaurante

--- resources/views/restorants/show.blade.php
+++ resources/views/restorants/show.blade.php
@@ -11,13 +11,7 @@
 @endsection
 
 @section('content')
-<?php
-    function clean($string) {
-        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
 
-        return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
-     }
-     ?>
 <style>
     .logo-text{
         float:left;
