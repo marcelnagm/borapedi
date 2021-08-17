20506f701223be1b52115b1d5840369cccc4af4f
Marcel Nagm <marcel.nagm@gmail.com>
2021-08-17 14:05:39 -0400

    Campo de troco

--- resources/views/cart/payment.blade.php
+++ resources/views/cart/payment.blade.php
@@ -36,7 +36,7 @@
                 <!-- COD -->
                 @if (!config('settings.hide_cod'))
                 <h5>Forma de Pagamento</h5>
-                <select id="paymentType" name="paymentType" class="">
+                <select id="paymentType" name="paymentType" class="" onchange="payment(this)">
                     <option value='' >Selecione uma forma de pagamento</option>
                     <option  class="custom-control-input" id="cashOnDelivery" value='cod' >Pagamento em Dinheiro - Entrega</option>
                     <option class="custom-control-input" id="cardOnDelivery" value='card'>Pagamento no Cartão- Entrega</option>
@@ -51,54 +51,77 @@
                     @endif
                 </select>
 
-                @endif
+                <div id="form-group-money_change" class="form-group  " style="display: none;">
+                    <label class="form-control-label" for="money_change">Troco para quanto?</label>
+                    <input type="number" name="money_change" id="money_change" class="form-control form-control " >
 
-                <!--                        
-                                             STIPE CART 
-                                            @if (config('settings.stripe_key')&&config('settings.enable_stripe'))
-                                                <div class="custom-control custom-radio mb-3">
-                                                    <input name="paymentType" class="custom-control-input" id="paymentStripe" type="radio" value="stripe" {{ config('settings.default_payment')=="stripe"?"checked":""}}>
-                                                    <label class="custom-control-label" for="paymentStripe">{{ __('Pay with card') }}</label>
-                                                </div>
-                                            @endif-->
+                    <div/>
+                    @endif
 
+                    <!--                        
+                                                 STIPE CART 
+                                                @if (config('settings.stripe_key')&&config('settings.enable_stripe'))
+                                                    <div class="custom-control custom-radio mb-3">
+                                                        <input name="paymentType" class="custom-control-input" id="paymentStripe" type="radio" value="stripe" {{ config('settings.default_payment')=="stripe"?"checked":""}}>
+                                                        <label class="custom-control-label" for="paymentStripe">{{ __('Pay with card') }}</label>
+                                                    </div>
+                                                @endif-->
+
+
+                </div>
+
+                <!-- END Payment -->
+
+                <!-- Payment Actions -->
+
+                <!-- COD -->
+                @include('cart.payments.cod')
+
+                <!-- Extra Payments ( Via module ) -->
+                @foreach ($extraPayments as $extraPayment)
+                @include($extraPayment.'::button')
+                @endforeach
+
+                </form>
+
+
+                <!-- END Payment Actions -->
 
             </div>
+        </div>   
 
-            <!-- END Payment -->
-
-            <!-- Payment Actions -->
-
-            <!-- COD -->
-            @include('cart.payments.cod')
-
-            <!-- Extra Payments ( Via module ) -->
-            @foreach ($extraPayments as $extraPayment)
-            @include($extraPayment.'::button')
-            @endforeach
-
-            </form>
-
-
-            <!-- END Payment Actions -->
-
+        @if (auth()->user()->phone != "")
+        <br/>
+        <div class="text-center " style="padding:15px 30px 0 30px; background-color: #d9d9d9">
+            <div class="custom-control custom-checkbox">
+                <input class="custom-control-input" id="privacypolicy" type="checkbox">
+                <label class="custom-control-label" for="privacypolicy" style="text-align:  justify;">
+                    <b > Concordar em receber mensagens do Whatsapp</b>
+                    <p>Você receberá notificações do estabelecimento por WhatsApp sobre o seu pedido.</p>
+                </label>        
+            </div>
         </div>
-    </div>   
-
-    @if (auth()->user()->phone != "")
-    <br/>
-    <div class="text-center " style="padding:15px 30px 0 30px; background-color: #d9d9d9">
-        <div class="custom-control custom-checkbox">
-            <input class="custom-control-input" id="privacypolicy" type="checkbox">
-            <label class="custom-control-label" for="privacypolicy" style="text-align:  justify;">
-                <b > Concordar em receber mensagens do Whatsapp</b>
-                <p>Você receberá notificações do estabelecimento por WhatsApp sobre o seu pedido.</p>
-            </label>        
-        </div>
+        @endif
     </div>
-    @endif
-</div>
 
-@if(config('settings.is_demo') && config('settings.enable_stripe'))
-@include('cart.democards')
-@endif
+    @if(config('settings.is_demo') && config('settings.enable_stripe'))
+    @include('cart.democards')
+    @endif
+
+    <script>
+        function payment(e) {
+            var optionSelected = $("option:selected", e);
+            var valueSelected = e.value;
+            if (valueSelected === "cod") {
+                $('#form-group-money_change').show();
+            } else {
+                $('#form-group-money_change').hide();
+            }
+        }
+
+
+        // function payment (e) {
+
+        //)
+
+    </script>
