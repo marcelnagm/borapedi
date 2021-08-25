35ed9c47e23e1e9abdd0b7b3bd7e6cea269e47a2
Marcel Nagm <marcel.nagm@gmail.com>
2021-08-25 13:21:42 -0400

    flash notify

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,7 +1,39 @@
 
 <script src="{{ asset('social') }}/js/core/jquery.min.js" type="text/javascript"></script>
 <script src="{{ asset('custom') }}/js/notify.min.js"></script>
-<script src="{{ asset('custom') }}/js/notify_custom.js"></script>
+<script>
+  function notify(text, type){
+        $.notify.addStyle('custom', {
+            html: "<div><strong><span data-notify-text /></strong></div>",
+            classes: {
+                base: {
+                    "pos ition": "relative",
+                    "margin-bottom": "1rem",
+                    "padding": "1rem 1.5rem",
+                    "border": "1px solid transparent",
+                    "border-radius": ".375rem",
+
+                    "color": "#fff",
+                    "border-color": type == "success" ? "#4fd69c" : "#fc7c5f",
+                    "background-color": type == "success" ? "#4fd69c" : "#fc7c5f",
+                },
+                success: {
+                    "color": "#fff",
+                    "border-color": type == "success" ? "#4fd69c" : "#fc7c5f",
+                    "background-color": type == "success" ? "#4fd69c" : "#fc7c5f",
+                }
+            }
+            });
+
+            $.notify(text,{
+                position: "top right",
+                style: 'custom',
+                className: 'success',
+                autoHideDelay: 15000
+            }
+        );
+    }
+   </script>
 @if (session('status'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         {{ session('status') }}
@@ -12,6 +44,7 @@
 <script>
 //    alert ('tem');
 notify('{{ session('status') }}','success');
+
    </script>
 @endif
 @if (session('error'))
